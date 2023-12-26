bdb3222d2697b8e3211650d8a56e706912e35a4a
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-26 17:10:18 -0300

    Rota categoria e rota livros implentadas

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,4 +1,7 @@
 from .categoriaClass import CategoriaView
 from .categoriaApiView import CategoriaDetail, CategoriasList
 from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
+
+from .autor import AutorViewSet
 from .categoria import CategoriaViewSet
+from .editora import EditoraViewSet
