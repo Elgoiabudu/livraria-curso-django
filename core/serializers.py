3dc11eb5e0f35851d833117ee09327437da371a8
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-27 09:51:09 -0300

    Lapidação da view de resposta da request de Livros

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from core.models import Categoria, Editora, Autor, Livro
 
@@ -9,10 +9,16 @@ class CategoriaSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class EditoraSerializer(ModelSerializer):
+class EditoraSerializer(ModelSerializer):  # Retorna o nome e id da editora
     class Meta:
         model = Editora
         fields = "__all__"
+
+
+class EditoraNestedSerializer(ModelSerializer):  # Somente o nome da editora
+    class Meta:
+        model = Editora
+        fields = ("nome",)
 
 
 class AutorSerializer(ModelSerializer):
@@ -25,3 +31,21 @@ class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = "__all__"
+
+
+class LivroDetailSerializer(ModelSerializer):
+    categoria = CharField(source="categoria.descricao")
+    editora = EditoraNestedSerializer()
+    # autores = SerializerMethodField() - Com essa adaptação, é retornado apenas o nome do(s) autor(es)
+
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1  # Exibe um nível a mais no rsponse.
+
+    def get_autores(self, instance):
+        nomes_autores = []
+        autores = instance.autores.get_queryset()
+        for autor in autores:
+            nomes_autores.append(autor.nome)
+        return nomes_autores
