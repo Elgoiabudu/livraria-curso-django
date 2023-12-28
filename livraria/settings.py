5c1bed4e527cd285ea19d38ca923c4a5c1e7ebb1
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-28 14:46:51 -0300

    Implementação do SimpleJwd do DjangoRestFrameWork

--- livraria/settings.py
+++ livraria/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "core",
 ]
 
@@ -111,3 +112,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
--- livraria/urls.py
+++ livraria/urls.py
@@ -5,15 +5,24 @@ from rest_framework import routers
 
 from core import views
 
+from rest_framework_simplejwt.views import ( # Implementação do JWT
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
 router = routers.DefaultRouter()
-router.register(r"categorias", views.CategoriaViewSet)
-router.register(r"editoras", views.EditoraViewSet)
 router.register(r"autores", views.AutorViewSet)
-router.register(r"livros", views.LivroViewSet)
+router.register(r"categorias", views.CategoriaViewSet)
 router.register(r"compras", views.CompraViewSet)
+router.register(r"editoras", views.EditoraViewSet)
+router.register(r"livros", views.LivroViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refresh Token
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'), # Verify Token
     path("categorias-class/", views.CategoriaView.as_view()),
     path("categorias-class/<int:id>", views.CategoriaView.as_view()),
     path("categorias-apiview", views.CategoriasList.as_view()),
