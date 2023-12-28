133321e21a1e4a007914b29b513264fd2d50bbe1
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-28 15:21:01 -0300

    Implementação do OpenAPI (Swagger/Redoc)

--- livraria/settings.py
+++ livraria/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_spectacular",
     "core",
 ]
 
@@ -120,4 +121,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Minha Livraria - API',
+    'DESCRIPTION': 'API com endpoints e documentação',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+    # OTHER SETTINGS
 }
--- livraria/urls.py
+++ livraria/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 from rest_framework import routers
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 from core import views
 
 from rest_framework_simplejwt.views import ( # Implementação do JWT
@@ -20,14 +22,21 @@ router.register(r"livros", views.LivroViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Autenticação
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Refresh Token
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'), # Verify Token
+    # OpenAPI 3
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),    
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Rotas Convencionais da API
     path("categorias-class/", views.CategoriaView.as_view()),
     path("categorias-class/<int:id>", views.CategoriaView.as_view()),
     path("categorias-apiview", views.CategoriasList.as_view()),
     path("categorias-apiview/<int:id>/", views.CategoriaDetail.as_view()),
     path("categorias-generic/", views.CategoriasListGeneric.as_view()),
     path("categorias-generic/<int:id>/", views.CategoriaDetailGeneric.as_view()),
+    #Rotas Funcionais distribuidas na API
     path("", include(router.urls)),
 ]
