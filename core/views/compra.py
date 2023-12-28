00e381d41a6d50eec1a3f0dea4ebb1b535c11cf7
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-28 16:22:03 -0300

    Definição das Roles's de filtro de pesquisa de compras

--- core/views/compra.py
+++ core/views/compra.py
@@ -7,8 +7,15 @@ from core.serializers import CompraSerializer, CriarEditarCompraSerializer
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
 
-    # serializer_class = CompraSerializer
     def get_serializer_class(self):
         if self.action == "list" or self.action == "retrieve":
             return CompraSerializer
         return CriarEditarCompraSerializer
+
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.groups.filter(
+            name="Administradores(Livraria)"
+        ) or usuario.groups.filter(name="Administradores(SuperUser)"):
+            return Compra.objects.all()
+        return Compra.objects.filter(usuario=usuario)
