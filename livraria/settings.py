605c7a64a769c81f226e2f6eb3e9601ca83e3a17
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-12-28 13:54:36 -0300

    iniciando tste de procução

--- livraria/settings.py
+++ livraria/settings.py
@@ -12,9 +12,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0*fe09s!6+eemxu)!atowjg0i&*tytlu1pq7%3-t&2-a8og0qa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
